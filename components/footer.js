5826b3a4247e1e9d1be1ce7378d775303057dc1b
imrishit98 <imrishit98@gmail.com>
2021-03-25 01:11:10 -0400

    fully resposive complete

--- components/footer.js
+++ components/footer.js
@@ -2,13 +2,14 @@ import Link from 'next/link';
 const Footer = () => {
   return (
     <footer>
-      <div className='links'>
+      {/* <div className='links'>
         <Link href='/'>Privacy Policy</Link>
         <Link href='/'>Terms and Conditions</Link>
       </div>
       <p>
         Made with 💜 in Ca<span>na</span>da
-      </p>
+      </p> */}
+      <h2>Youfy</h2>
       <a href='/'>Loved Youfy? Buy us a coffee!</a>
     </footer>
   );
