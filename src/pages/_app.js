dd98351a4bbb602ab2d1355a8980a48f8c046efc
Rishit <imrishit98@gmail.com>
2022-09-04 00:55:55 -0400

    added GTM

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,12 @@
+import { useEffect } from 'react';
+import TagManager from 'react-gtm-module';
 import '../styles/main.css';
 
 const MyApp = ({ Component, pageProps }) => {
+  useEffect(() => {
+    TagManager.initialize({ gtmId: 'GTM-MSD3FCS' });
+  }, []);
+
   return <Component {...pageProps} />;
 };
 
