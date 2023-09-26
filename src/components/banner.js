10bdb8adb5f8ab475632faf5180772c89f43d50d
Rishit <imrishit98@gmail.com>
2023-09-25 20:05:36 -0400

    Change the sponsor banner to PDFPeer

--- src/components/banner.js
+++ src/components/banner.js
@@ -1,11 +1,15 @@
-
 export const AnnouncementBanner = () => {
   return (
     <div className='banner'>
-      ✨ Deep dive in the world of AI with{' '}
+      ✨ Turn your documents to AI with{' '}
       <div className='link'>
-        <a href={'https://aitools.fyi/'} target='_blank'>
-          aitools.fyi
+        <a
+          href={
+            'https://pdfpeer.com?ref=youfy&utm_source=youfy&utm_medium=banner&utm_campaign=youfy'
+          }
+          target='_blank'
+        >
+          PDFPeer
         </a>
       </div>
     </div>
